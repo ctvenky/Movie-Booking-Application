1bd653335ba3be1bd285cda3f706ce1803e910e7
ctvenky <137060454+ctvenky@users.noreply.github.com>
2023-09-11 04:51:29 +0530

    Delete routes directory

--- routes/genre.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const genreController = require('../controllers/genre.controller');
-
-router.get('/genres', genreController.findAllGenres);
-
-module.exports = router;
